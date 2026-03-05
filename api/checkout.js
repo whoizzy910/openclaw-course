56ebfb7030b1bbb1ca85072668272d7f45bb6c21
Dj IZZYNICE? <djizzynice@Djs-MacBook-Pro.local>
2026-03-04 22:53:56 -0500

    Fix live price ID

--- api/checkout.js
+++ api/checkout.js
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-const PRICE_ID = "price_1T7S9iCbCTfrLA4u0absHM3c";
+const PRICE_ID = "price_1T7Sw5Lt8E5SgMjKjiyDth1y";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
